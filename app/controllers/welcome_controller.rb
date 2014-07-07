acd3ceb9f5768c0d293dd1a3056cf4636406fef5
danid123 <danid123@users.noreply.github.com>
2014-07-07 10:34:13 -0700

    Adding nav bar and sidebar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   	@articles = Article.order(updated_at: :desc).limit(5)
+  	@categories = Category.order("name")
+  end
+
+  def show
+  	@articles = Category.find(params[:id]).articles.order(created_at: :desc)
   end
 end
