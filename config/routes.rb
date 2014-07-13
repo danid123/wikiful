0a88e4c876b3854a239d35245ad2396328d1f1da
danid123 <danid123@users.noreply.github.com>
2014-07-13 14:42:19 -0700

    Adding user model and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -9,6 +11,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :articles
   resources :categories
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
